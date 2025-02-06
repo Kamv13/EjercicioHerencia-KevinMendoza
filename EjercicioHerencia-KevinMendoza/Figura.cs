28a8fcede9c097fba9dd8efbbc6d2ba93868d6e2
Kamv13 <ksmv233@gmail.com>
2025-02-06 12:42:43 -0600

    finalizacion de clases

---
+++ EjercicioHerencia-KevinMendoza/Figura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioHerencia_KevinMendoza
+{
+    public class Figura
+    {
+        public double? Base {  get; set; }
+        public double? Altura { get; set; }
+        public string? tipo { get; set; }
+    }
+}
---
+++ EjercicioHerencia-KevinMendoza/Operaciones.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+namespace EjercicioHerencia_KevinMendoza
+{
+    public class Operaciones : Figura
+     {
+        public const double Const1 = 0.5;
+
+        public double AreaTriangulo() {
+            return (double)(Const1 * (Base * Altura));
+        }
+
+        public void imprimir() {
+
+            Console.WriteLine($"La figura es un {tipo}");
+            Console.WriteLine($"El area del {tipo} es {AreaTriangulo}");
+        }
+     
+    }
+}
